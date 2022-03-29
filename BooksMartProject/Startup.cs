061972a292197e8b0521785fef5ffe4a6dad3073
Kawsar Rashik <kawsar.rashik@bizzntek.com>
2022-03-29 12:38:38 +0600

    Creating and configuring ViewStart and imports

--- BooksMartProject/Startup.cs
+++ BooksMartProject/Startup.cs
@@ -64,7 +64,7 @@ namespace BooksMartProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
